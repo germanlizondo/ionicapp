e7e7c5f5dc2f2654d249ceb86910de1c00adc660
germanlizondo <german9lp@gmail.com>
2019-01-03 22:58:32 +0100

    hellothere

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,6 +37,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  http;
+  paises:any[] = [];
+  constructor(public navCtrl: NavController,_http: HttpClient) {
+    this.http = _http;
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  abrirpuerta(){
+   this.http.get('https://restcountries.eu/rest/v2/lang/es')
+   .subscribe(data => {
+ console.log(data);
+  this.paises = data
+console.log(this.paises);
+  }, err => {
+    this.paises = [{
+      "name": "error"
+    }]
+    console.log(err);
+  });
   }
 
 }
